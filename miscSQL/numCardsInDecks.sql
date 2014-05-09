4e323a6bca4f439001e9290673dc4387d48a7ef4
Jen Larkin <jlarkin@gmail.com>
2014-05-09 15:37:13 -0700

    adding custom deck exports for testing

---
+++ miscSQL/numCardsInDecks.sql
@@ -0,0 +1,8 @@
+﻿select card_set.name, count(card_set_white_card.white_card_id) as numWhiteCards
+from card_set join card_set_white_card on card_set.id = card_set_white_card.card_set_id
+group by card_set.name
+
+
+select card_set.name, count(card_set_black_card.black_card_id) as numBlackCards
+from card_set join card_set_black_card on card_set.id = card_set_black_card.card_set_id
+group by card_set.name
